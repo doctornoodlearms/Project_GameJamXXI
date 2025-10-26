7514637c2d2cf0877a1a44ee7be1caf2cfd6ec58
Doctornoodlearms <doctornoodlearms11@gmail.com>
2025-10-26 13:29:28 -0400

    fixed potion crafting

--- Scenes/Instances/OrbInterface.cs
+++ Scenes/Instances/OrbInterface.cs
@@ -50,22 +50,26 @@ public partial class OrbInterface : Control{
 
             case(PotionHeatStroke.Name):
                 text = $"[pulse][color={PotionHeatStroke.PotionColor.ToHtml()}][b]{PotionHeatStroke.DisplayName}[/b][/color][/pulse]";
-                text += "\nIngredients: "+ PotionHeatStroke.IngredientsText;
+                text += "\n\nDescription: " + PotionHeatStroke.Description;
+                text += "\n\nIngredients: "+ PotionHeatStroke.IngredientsText;
                 break;
 
             case(PotionHypothermia.Name):
                 text = $"[shake][color={PotionHypothermia.PotionColor.ToHtml()}][b]{PotionHypothermia.DisplayName}[/b][/color][/shake]";
-                text += "\nIngredients: " + PotionHypothermia.IngredientsText;
+                text += "\n\nDescription: " + PotionHypothermia.Description;
+                text += "\n\nIngredients: " + PotionHypothermia.IngredientsText;
                 break;
             
             case(PotionStabbed.Name):
                 text = $"[wave][color={PotionStabbed.PotionColor.ToHtml()}][b]{PotionStabbed.DisplayName}[/b][/color][/wave]";
-                text += "\nIngredients: " + PotionStabbed.IngredientsText;
+                text += "\n\nDescription: " + PotionStabbed.Description;
+                text += "\n\nIngredients: " + PotionStabbed.IngredientsText;
                 break;
 
             case(PotionSick.Name):
                 text = $"[tornado][color={PotionSick.PotionColor.ToHtml()}][b]{PotionSick.DisplayName}[/b][/color][/tornado]";
-                text += "\nIngredients: " + PotionSick.IngredientsText;
+                text += "\n\nDescription: " + PotionSick.Description;
+                text += "\n\nIngredients: " + PotionSick.IngredientsText;
                 break;
         }
 
--- Scenes/Instances/Shelf.cs
+++ Scenes/Instances/Shelf.cs
@@ -4,10 +4,13 @@ using Scenes.Instances;
 public partial class Shelf : StaticBody2D {
 
     [Export] Node2D IngredientSpawn;
+    [Export] Node2D IngredientSpawn2;
 
     [Export] string ingredientName;
+    [Export] string ingredientName2;
 
-    Ingredient ingredientRef;
+    Ingredient ingredientRef1;
+    Ingredient ingredientRef2;
 
     PackedScene ingredientInstance;
 
@@ -23,14 +26,24 @@ public partial class Shelf : StaticBody2D {
 
     public void RespawnIngredient(){
 
-        if(IsInstanceValid(ingredientRef)){
+        if(IsInstanceValid(ingredientRef1)){
 
-            ingredientRef.QueueFree();
+            ingredientRef1.QueueFree();
         }
 
         Ingredient instance = ingredientInstance.Instantiate<Ingredient>();
         instance.ingredientName = ingredientName;
-        ingredientRef = instance;
+        ingredientRef1 = instance;
         IngredientSpawn.AddChild(instance);
+
+        if(IsInstanceValid(ingredientRef2)){
+
+            ingredientRef2.QueueFree();
+        }
+
+        Ingredient instance2 = ingredientInstance.Instantiate<Ingredient>();
+        instance2.ingredientName = ingredientName2;
+        ingredientRef2 = instance2;
+        IngredientSpawn2.AddChild(instance2);
     }
 }
